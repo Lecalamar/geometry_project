92900d0dfe073891bdd5a6b639e77d0479a6843c
root <root@pc-nico.localhost>
2016-10-15 19:05:33 +0200

    projet de base

---
+++ geomod-project/moc_mainWidget.cpp
@@ -0,0 +1,185 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'mainWidget.h'
+**
+** Created by: The Qt Meta Object Compiler version 67 (Qt 5.5.1)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include "mainWidget.h"
+#include <QtCore/qbytearray.h>
+#include <QtCore/qmetatype.h>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'mainWidget.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 67
+#error "This file was generated using the moc from 5.5.1. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+QT_BEGIN_MOC_NAMESPACE
+struct qt_meta_stringdata_MainWindow_t {
+    QByteArrayData data[20];
+    char stringdata0[151];
+};
+#define QT_MOC_LITERAL(idx, ofs, len) \
+    Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
+    qptrdiff(offsetof(qt_meta_stringdata_MainWindow_t, stringdata0) + ofs \
+        - idx * sizeof(QByteArrayData)) \
+    )
+static const qt_meta_stringdata_MainWindow_t qt_meta_stringdata_MainWindow = {
+    {
+QT_MOC_LITERAL(0, 0, 10), // "MainWindow"
+QT_MOC_LITERAL(1, 11, 7), // "newFile"
+QT_MOC_LITERAL(2, 19, 0), // ""
+QT_MOC_LITERAL(3, 20, 4), // "open"
+QT_MOC_LITERAL(4, 25, 4), // "save"
+QT_MOC_LITERAL(5, 30, 6), // "saveAs"
+QT_MOC_LITERAL(6, 37, 8), // "settings"
+QT_MOC_LITERAL(7, 46, 4), // "copy"
+QT_MOC_LITERAL(8, 51, 5), // "paste"
+QT_MOC_LITERAL(9, 57, 9), // "selectAll"
+QT_MOC_LITERAL(10, 67, 4), // "help"
+QT_MOC_LITERAL(11, 72, 5), // "about"
+QT_MOC_LITERAL(12, 78, 10), // "closeAppli"
+QT_MOC_LITERAL(13, 89, 9), // "nextFrame"
+QT_MOC_LITERAL(14, 99, 9), // "prevFrame"
+QT_MOC_LITERAL(15, 109, 10), // "firstFrame"
+QT_MOC_LITERAL(16, 120, 9), // "lastFrame"
+QT_MOC_LITERAL(17, 130, 4), // "play"
+QT_MOC_LITERAL(18, 135, 4), // "stop"
+QT_MOC_LITERAL(19, 140, 10) // "timerEvent"
+
+    },
+    "MainWindow\0newFile\0\0open\0save\0saveAs\0"
+    "settings\0copy\0paste\0selectAll\0help\0"
+    "about\0closeAppli\0nextFrame\0prevFrame\0"
+    "firstFrame\0lastFrame\0play\0stop\0"
+    "timerEvent"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_MainWindow[] = {
+
+ // content:
+       7,       // revision
+       0,       // classname
+       0,    0, // classinfo
+      18,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+ // slots: name, argc, parameters, tag, flags
+       1,    0,  104,    2, 0x08 /* Private */,
+       3,    0,  105,    2, 0x08 /* Private */,
+       4,    0,  106,    2, 0x08 /* Private */,
+       5,    0,  107,    2, 0x08 /* Private */,
+       6,    0,  108,    2, 0x08 /* Private */,
+       7,    0,  109,    2, 0x08 /* Private */,
+       8,    0,  110,    2, 0x08 /* Private */,
+       9,    0,  111,    2, 0x08 /* Private */,
+      10,    0,  112,    2, 0x08 /* Private */,
+      11,    0,  113,    2, 0x08 /* Private */,
+      12,    0,  114,    2, 0x08 /* Private */,
+      13,    0,  115,    2, 0x08 /* Private */,
+      14,    0,  116,    2, 0x08 /* Private */,
+      15,    0,  117,    2, 0x08 /* Private */,
+      16,    0,  118,    2, 0x08 /* Private */,
+      17,    0,  119,    2, 0x08 /* Private */,
+      18,    0,  120,    2, 0x08 /* Private */,
+      19,    0,  121,    2, 0x08 /* Private */,
+
+ // slots: parameters
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Bool,
+    QMetaType::Bool,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Bool,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+
+       0        // eod
+};
+
+void MainWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        MainWindow *_t = static_cast<MainWindow *>(_o);
+        Q_UNUSED(_t)
+        switch (_id) {
+        case 0: _t->newFile(); break;
+        case 1: _t->open(); break;
+        case 2: { bool _r = _t->save();
+            if (_a[0]) *reinterpret_cast< bool*>(_a[0]) = _r; }  break;
+        case 3: { bool _r = _t->saveAs();
+            if (_a[0]) *reinterpret_cast< bool*>(_a[0]) = _r; }  break;
+        case 4: _t->settings(); break;
+        case 5: _t->copy(); break;
+        case 6: _t->paste(); break;
+        case 7: _t->selectAll(); break;
+        case 8: _t->help(); break;
+        case 9: _t->about(); break;
+        case 10: { bool _r = _t->closeAppli();
+            if (_a[0]) *reinterpret_cast< bool*>(_a[0]) = _r; }  break;
+        case 11: _t->nextFrame(); break;
+        case 12: _t->prevFrame(); break;
+        case 13: _t->firstFrame(); break;
+        case 14: _t->lastFrame(); break;
+        case 15: _t->play(); break;
+        case 16: _t->stop(); break;
+        case 17: _t->timerEvent(); break;
+        default: ;
+        }
+    }
+}
+
+const QMetaObject MainWindow::staticMetaObject = {
+    { &QMainWindow::staticMetaObject, qt_meta_stringdata_MainWindow.data,
+      qt_meta_data_MainWindow,  qt_static_metacall, Q_NULLPTR, Q_NULLPTR}
+};
+
+
+const QMetaObject *MainWindow::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *MainWindow::qt_metacast(const char *_clname)
+{
+    if (!_clname) return Q_NULLPTR;
+    if (!strcmp(_clname, qt_meta_stringdata_MainWindow.stringdata0))
+        return static_cast<void*>(const_cast< MainWindow*>(this));
+    return QMainWindow::qt_metacast(_clname);
+}
+
+int MainWindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QMainWindow::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 18)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 18;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 18)
+            *reinterpret_cast<int*>(_a[0]) = -1;
+        _id -= 18;
+    }
+    return _id;
+}
+QT_END_MOC_NAMESPACE
---
+++ geomod-project/moc_toolsWidget.cpp
@@ -0,0 +1,225 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'toolsWidget.h'
+**
+** Created by: The Qt Meta Object Compiler version 67 (Qt 5.5.1)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include "toolsWidget.h"
+#include <QtCore/qbytearray.h>
+#include <QtCore/qmetatype.h>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'toolsWidget.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 67
+#error "This file was generated using the moc from 5.5.1. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+QT_BEGIN_MOC_NAMESPACE
+struct qt_meta_stringdata_ToolsWidget_t {
+    QByteArrayData data[29];
+    char stringdata0[433];
+};
+#define QT_MOC_LITERAL(idx, ofs, len) \
+    Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
+    qptrdiff(offsetof(qt_meta_stringdata_ToolsWidget_t, stringdata0) + ofs \
+        - idx * sizeof(QByteArrayData)) \
+    )
+static const qt_meta_stringdata_ToolsWidget_t qt_meta_stringdata_ToolsWidget = {
+    {
+QT_MOC_LITERAL(0, 0, 11), // "ToolsWidget"
+QT_MOC_LITERAL(1, 12, 19), // "createButtonClicked"
+QT_MOC_LITERAL(2, 32, 0), // ""
+QT_MOC_LITERAL(3, 33, 17), // "editButtonClicked"
+QT_MOC_LITERAL(4, 51, 16), // "curveTypeChanged"
+QT_MOC_LITERAL(5, 68, 15), // "penColorClicked"
+QT_MOC_LITERAL(6, 84, 17), // "brushColorClicked"
+QT_MOC_LITERAL(7, 102, 15), // "penWidthChanged"
+QT_MOC_LITERAL(8, 118, 15), // "penColorChanged"
+QT_MOC_LITERAL(9, 134, 17), // "brushColorChanged"
+QT_MOC_LITERAL(10, 152, 11), // "playClicked"
+QT_MOC_LITERAL(11, 164, 11), // "stopClicked"
+QT_MOC_LITERAL(12, 176, 11), // "prevClicked"
+QT_MOC_LITERAL(13, 188, 11), // "nextClicked"
+QT_MOC_LITERAL(14, 200, 12), // "frameChanged"
+QT_MOC_LITERAL(15, 213, 18), // "addPtBeforeClicked"
+QT_MOC_LITERAL(16, 232, 17), // "addPtAfterClicked"
+QT_MOC_LITERAL(17, 250, 12), // "delPtClicked"
+QT_MOC_LITERAL(18, 263, 11), // "sizeChanged"
+QT_MOC_LITERAL(19, 275, 15), // "nbFramesChanged"
+QT_MOC_LITERAL(20, 291, 10), // "fpsChanged"
+QT_MOC_LITERAL(21, 302, 15), // "kfModePtClicked"
+QT_MOC_LITERAL(22, 318, 18), // "kfModeCurveClicked"
+QT_MOC_LITERAL(23, 337, 18), // "kfModeSceneClicked"
+QT_MOC_LITERAL(24, 356, 12), // "kfAddClicked"
+QT_MOC_LITERAL(25, 369, 12), // "kfDelClicked"
+QT_MOC_LITERAL(26, 382, 15), // "kfNextPtClicked"
+QT_MOC_LITERAL(27, 398, 15), // "kfPrevPtClicked"
+QT_MOC_LITERAL(28, 414, 18) // "kfApplyTypeClicked"
+
+    },
+    "ToolsWidget\0createButtonClicked\0\0"
+    "editButtonClicked\0curveTypeChanged\0"
+    "penColorClicked\0brushColorClicked\0"
+    "penWidthChanged\0penColorChanged\0"
+    "brushColorChanged\0playClicked\0stopClicked\0"
+    "prevClicked\0nextClicked\0frameChanged\0"
+    "addPtBeforeClicked\0addPtAfterClicked\0"
+    "delPtClicked\0sizeChanged\0nbFramesChanged\0"
+    "fpsChanged\0kfModePtClicked\0"
+    "kfModeCurveClicked\0kfModeSceneClicked\0"
+    "kfAddClicked\0kfDelClicked\0kfNextPtClicked\0"
+    "kfPrevPtClicked\0kfApplyTypeClicked"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_ToolsWidget[] = {
+
+ // content:
+       7,       // revision
+       0,       // classname
+       0,    0, // classinfo
+      27,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+ // slots: name, argc, parameters, tag, flags
+       1,    0,  149,    2, 0x0a /* Public */,
+       3,    0,  150,    2, 0x0a /* Public */,
+       4,    1,  151,    2, 0x0a /* Public */,
+       5,    0,  154,    2, 0x0a /* Public */,
+       6,    0,  155,    2, 0x0a /* Public */,
+       7,    1,  156,    2, 0x0a /* Public */,
+       8,    1,  159,    2, 0x0a /* Public */,
+       9,    1,  162,    2, 0x0a /* Public */,
+      10,    0,  165,    2, 0x0a /* Public */,
+      11,    0,  166,    2, 0x0a /* Public */,
+      12,    0,  167,    2, 0x0a /* Public */,
+      13,    0,  168,    2, 0x0a /* Public */,
+      14,    1,  169,    2, 0x0a /* Public */,
+      15,    0,  172,    2, 0x0a /* Public */,
+      16,    0,  173,    2, 0x0a /* Public */,
+      17,    0,  174,    2, 0x0a /* Public */,
+      18,    0,  175,    2, 0x0a /* Public */,
+      19,    1,  176,    2, 0x0a /* Public */,
+      20,    1,  179,    2, 0x0a /* Public */,
+      21,    0,  182,    2, 0x0a /* Public */,
+      22,    0,  183,    2, 0x0a /* Public */,
+      23,    0,  184,    2, 0x0a /* Public */,
+      24,    0,  185,    2, 0x0a /* Public */,
+      25,    0,  186,    2, 0x0a /* Public */,
+      26,    0,  187,    2, 0x0a /* Public */,
+      27,    0,  188,    2, 0x0a /* Public */,
+      28,    0,  189,    2, 0x0a /* Public */,
+
+ // slots: parameters
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::QString,    2,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::Int,    2,
+    QMetaType::Void, QMetaType::QColor,    2,
+    QMetaType::Void, QMetaType::QColor,    2,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::Int,    2,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::Int,    2,
+    QMetaType::Void, QMetaType::Int,    2,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+
+       0        // eod
+};
+
+void ToolsWidget::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        ToolsWidget *_t = static_cast<ToolsWidget *>(_o);
+        Q_UNUSED(_t)
+        switch (_id) {
+        case 0: _t->createButtonClicked(); break;
+        case 1: _t->editButtonClicked(); break;
+        case 2: _t->curveTypeChanged((*reinterpret_cast< const QString(*)>(_a[1]))); break;
+        case 3: _t->penColorClicked(); break;
+        case 4: _t->brushColorClicked(); break;
+        case 5: _t->penWidthChanged((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 6: _t->penColorChanged((*reinterpret_cast< const QColor(*)>(_a[1]))); break;
+        case 7: _t->brushColorChanged((*reinterpret_cast< const QColor(*)>(_a[1]))); break;
+        case 8: _t->playClicked(); break;
+        case 9: _t->stopClicked(); break;
+        case 10: _t->prevClicked(); break;
+        case 11: _t->nextClicked(); break;
+        case 12: _t->frameChanged((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 13: _t->addPtBeforeClicked(); break;
+        case 14: _t->addPtAfterClicked(); break;
+        case 15: _t->delPtClicked(); break;
+        case 16: _t->sizeChanged(); break;
+        case 17: _t->nbFramesChanged((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 18: _t->fpsChanged((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 19: _t->kfModePtClicked(); break;
+        case 20: _t->kfModeCurveClicked(); break;
+        case 21: _t->kfModeSceneClicked(); break;
+        case 22: _t->kfAddClicked(); break;
+        case 23: _t->kfDelClicked(); break;
+        case 24: _t->kfNextPtClicked(); break;
+        case 25: _t->kfPrevPtClicked(); break;
+        case 26: _t->kfApplyTypeClicked(); break;
+        default: ;
+        }
+    }
+}
+
+const QMetaObject ToolsWidget::staticMetaObject = {
+    { &QWidget::staticMetaObject, qt_meta_stringdata_ToolsWidget.data,
+      qt_meta_data_ToolsWidget,  qt_static_metacall, Q_NULLPTR, Q_NULLPTR}
+};
+
+
+const QMetaObject *ToolsWidget::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *ToolsWidget::qt_metacast(const char *_clname)
+{
+    if (!_clname) return Q_NULLPTR;
+    if (!strcmp(_clname, qt_meta_stringdata_ToolsWidget.stringdata0))
+        return static_cast<void*>(const_cast< ToolsWidget*>(this));
+    return QWidget::qt_metacast(_clname);
+}
+
+int ToolsWidget::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QWidget::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 27)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 27;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 27)
+            *reinterpret_cast<int*>(_a[0]) = -1;
+        _id -= 27;
+    }
+    return _id;
+}
+QT_END_MOC_NAMESPACE
